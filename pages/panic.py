5b61ac1d22c82af60e915286d708d9aab2433650
zhaoweibing <zhaoweibing@zhuanzhuan.com>
2023-04-12 19:38:52 +0800

    update panic parser

--- pages/panic.py
+++ pages/panic.py
@@ -2,6 +2,11 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 
+# 需要关注的字符串信息，因为换行问题，需要单独检测拼接到上一行信息中
+focus_infos = [
+   "is_alive_func returned unhealthy",
+   "wifid has not exited since first loaded"
+]
 uploaded_excel_file_all = st.file_uploader("上传云窗Panic日志", type=["xlsx", "txt"])
 
 @st.cache_data
@@ -20,15 +25,15 @@ panic_string_column = dataframe["original_data"]
 id_column = dataframe["id"]
 for i in range(len(id_column)):
   id_string = str(id_column.iloc[i])
-  if "is_alive_func returned unhealthy" in id_string:
-    # modify the panic_string_column in i-1 location
-    panic_string_column.iloc[i-1] = panic_string_column.iloc[i-1] + " " + id_string
+  # loop check info in focus_infos in id_string
+  for info in focus_infos:
+    if info in id_string:
+      # modify the panic_string_column in i-1 location
+      panic_string_column.iloc[i-1] = panic_string_column.iloc[i-1] + " " + id_string
 
 # get panic_string_columns number
 panic_string_column = panic_string_column.dropna()
 panic_string_column_number = len(panic_string_column)
-st.write("### Panic日志数量: " + str(panic_string_column_number))
-st.write(panic_string_column)
 
 panic_string_arr = []
 for i in range(panic_string_column_number):
@@ -41,7 +46,7 @@ for i in range(panic_string_column_number):
 panic_string_arr = list(set(panic_string_arr))
 
 # convert panic_string_arr to dataframe
-st.write("### Panic日志去重后数量: " + str(len(panic_string_arr)))
+st.write("### Panic日志: " + str(len(panic_string_arr)))
 panic_string_df = pd.DataFrame(panic_string_arr, columns=["panic_string"])
 st.write(panic_string_df)
 
